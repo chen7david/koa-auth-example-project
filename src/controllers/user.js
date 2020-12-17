c6c0bb759ddc4bbffb432c9b9ced8b65785b01ab
David Chen <chen7david@me.com>
2020-12-17 11:12:30 +0800

    update

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,4 +1,4 @@
-
+const { User } = require('./../models')
 
 module.exports = {
     index: async (ctx) => {
--- src/router/user.js
+++ src/router/user.js
@@ -3,5 +3,5 @@ const controller = require('./../controllers').user
 const { validateBody, schema } = require('./../middleware/validate')
 
 router.get('/users', controller.index)
-router.get('/register', validateBody(schema.role), controller.index)
+router.post('/register', validateBody(schema.role), controller.index)
 module.exports = router 
